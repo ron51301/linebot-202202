e091bcbb38965e5162cbd8562ebffa34a64f784e
SilentNignht <jason830410@gmail.com>
2022-02-09 20:31:31 +0800

    update app

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ def pushMessage(payload):
 
 
 def getTotalSentMessageCount():
-    response = {}
+    response = requests.get("GET https://api.line.me/v2/bot/message/quota/consumption",headers="Authorization: Bearer {channelAccessToken}",data=json.dumps(payload))
     return 0
 
 
