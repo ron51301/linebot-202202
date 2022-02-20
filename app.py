8dd99a4f979451068bb119c6165dc20423561055
SilentNignht <jason830410@gmail.com>
2022-02-20 15:58:34 +0800

    test

--- app.py
+++ app.py
@@ -183,7 +183,8 @@ def daka():
     connection = pymysql.connect(host="us-cdbr-east-05.cleardb.net",
                                  user="b809ff374c792c",
                                  password="bbc8de98",
-                                 database="heroku_9a97caadd884ab8")
+                                 database="heroku_9a97caadd884ab8",
+                                 cursorclass=pymysql.cursors.DictCursor)
 
     cursor = connection.cursor()
     create_date = datetime.today().strftime('%Y-%m-%d')  # 得到當前日期
